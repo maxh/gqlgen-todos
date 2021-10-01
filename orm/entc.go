c18925bd74ae462fd647275436bd98024623dfff
Max Heinritz <maxheinritz@gmail.com>
2021-09-30 20:32:56 -0500

    try ent

---
+++ orm/entc.go
@@ -0,0 +1,24 @@
+// +build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+	"entgo.io/contrib/entgql"
+)
+
+func main() {
+	//ex, err := entgql.NewExtension()
+	//if err != nil {
+	//	log.Fatalf("creating entgql extension: %v", err)
+	//}
+	c := &gen.Config{
+		Target: "ent",
+	}
+	if err := entc.Generate("./schema", c); err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
