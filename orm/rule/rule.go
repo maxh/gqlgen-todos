55685b9b90b6ec38c98d3750f93036436ce7b2a0
Max Heinritz <maxheinritz@gmail.com>
2021-10-02 14:49:10 -0500

    add rule

---
+++ orm/rule/rule.go
@@ -0,0 +1,33 @@
+package rule
+
+import (
+	"context"
+
+	"entgo.io/ent/examples/privacyadmin/ent/privacy"
+	"entgo.io/ent/examples/privacyadmin/viewer"
+)
+
+// DenyIfNoViewer is a rule that returns Deny decision if the viewer is
+// missing in the context.
+func DenyIfNoViewer() privacy.QueryMutationRule {
+	return privacy.ContextQueryMutationRule(func(ctx context.Context) error {
+		view := viewer.FromContext(ctx)
+		if view == nil {
+			return privacy.Denyf("viewer-context is missing")
+		}
+		// Skip to the next privacy rule (equivalent to returning nil).
+		return privacy.Skip
+	})
+}
+
+// AllowIfAdmin is a rule that returns Allow decision if the viewer is admin.
+func AllowIfAdmin() privacy.QueryMutationRule {
+	return privacy.ContextQueryMutationRule(func(ctx context.Context) error {
+		view := viewer.FromContext(ctx)
+		if view.Admin() {
+			return privacy.Allow
+		}
+		// Skip to the next privacy rule (equivalent to returning nil).
+		return privacy.Skip
+	})
+}
